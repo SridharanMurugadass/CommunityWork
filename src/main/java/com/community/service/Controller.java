c31a83d3fcdaf5f0ff2eb4635e65f1ad12bc8a12
Sridharan Murugadass <sridharanbe.ece@gmail.com>
2018-10-11 14:28:44 +0530

    Update Controller.java

--- src/main/java/com/community/service/Controller.java
+++ src/main/java/com/community/service/Controller.java
@@ -1,7 +1,9 @@
 package com.community.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
-
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,20 @@ public List<test> getData() {
 		
 		return cc;
 		 
+	}
+	@GetMapping("/weather")
+	public String getWeather() throws IOException {
+
+		 ClassLoader classLoader = getClass().getClassLoader();
+		    File file = new File(classLoader.getResource("test.json").getFile());
+		    String data = FileUtils.readFileToString(file, "UTF-8");
+	 
+
+     System.out.println(data);        
+		
+
+		return data;
+
 	}
 
 }
